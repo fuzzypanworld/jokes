db834bde93bad0d0b3f8feae64241cbc3ee56963
Atharva Kumar <atharvapraveenkumar@gmail.com>
2024-09-18 12:13:54 +0530

    new commit

--- app/root.tsx
+++ app/root.tsx
@@ -1,30 +1,44 @@
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
-  Meta,
+  LiveReload,
   Outlet,
-  Scripts,
-  ScrollRestoration,
 } from "@remix-run/react";
-import "./tailwind.css";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+import globalLargeStylesUrl from "~/styles/global-large.css";
+import globalMediumStylesUrl from "~/styles/global-medium.css";
+import globalStylesUrl from "~/styles/global.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: globalStylesUrl },
+  {
+    rel: "stylesheet",
+    href: globalMediumStylesUrl,
+    media: "print, (min-width: 640px)",
+  },
+  {
+    rel: "stylesheet",
+    href: globalLargeStylesUrl,
+    media: "screen and (min-width: 1024px)",
+  },
+];
+
+export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <title>Remix: So great, it's funny!</title>
         <Links />
       </head>
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+        <Outlet />
+        <LiveReload />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
